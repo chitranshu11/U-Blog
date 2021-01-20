6fea9c91e8c81f872598d2e856a1f37793948fff
Chitranshu Gour <chitranshugour67@gmail.com>
2021-01-20 12:37:54 +0530

    Checkpoint 2

--- java-ublog/src/com/upgrad/ublog/exceptions/IncorrectPasswordException.java
+++ java-ublog/src/com/upgrad/ublog/exceptions/IncorrectPasswordException.java
@@ -1,24 +1,8 @@
 package com.upgrad.ublog.exceptions;
 
-/**
- * TODO: 2.1. Convert this class to a custom Exception class.
- * TODO: 2.2. Provide a constructor which accepts a custom message as input
- *  parameter and passes it to its base class.
- */
+public class IncorrectPasswordException extends Exception {
+    public IncorrectPasswordException(String message) {
+        super(message);
+    }
 
-public class IncorrectPasswordException {
-
-
-//    public static void main(String[] args) {
-//        try {
-//            throw new IncorrectPasswordException("Custom Message");
-//        } catch (IncorrectPasswordException e) {
-//            System.out.println(e.getMessage());
-//        }
-//
-//        /**
-//         * Your output should look like this.
-//         * Custom Message
-//         */
-//    }
 }
